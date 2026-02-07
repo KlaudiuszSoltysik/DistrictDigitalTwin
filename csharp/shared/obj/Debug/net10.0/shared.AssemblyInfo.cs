15871d24bb3f8ed6c27ce99e083587bccc88b0cc
KlaudiuszSoltysik <klaudiusz.s1405@gmail.com>
2026-02-07 22:22:21 +0100

    Add .gitignore files for history_service and shared projects; update AssemblyInfo version attributes

--- csharp/shared/obj/Debug/net10.0/shared.AssemblyInfo.cs
+++ csharp/shared/obj/Debug/net10.0/shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883db322126dc819d1226bef42807544aaa1574a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5185bd4670f9b5f48f5e3a48569d37fd1d44e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
